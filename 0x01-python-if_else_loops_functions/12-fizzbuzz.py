840941ffc5b8833e62f420f8458b786c3f05c70c
zakaria1011 <grarizakaria@gmail.com>
2023-10-03 23:49:46 +0100

    alx

---
+++ 0x01-python-if_else_loops_functions/12-fizzbuzz.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def fizzbuzz():
+    for n in range(1, 101):
+        if n % 5 == 0 and n % 3 == 0:
+            print("fizzbuzz", end=' ')
+        elif n % 5 == 0:
+            print("buzz", end=' ')
+        elif n % 3 == 0:
+            print("fizze", end=' ')
+        else:
+            print(n, end=' ')
