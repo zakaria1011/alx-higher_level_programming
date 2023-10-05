3f9d22ba65d8c84f104ca48120bc1919d4812d54
zakaria1011 <grarizakaria@gmail.com>
2023-10-05 11:45:02 +0100

    alx

--- 0x02-python-import_modules/0-add.py
+++ 0x02-python-import_modules/0-add.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
 from add_0 import add
-if __name__ == "__main__":
 a = 1
 b = 2
 result = a + b
 
 print("{} + {} = {}".format(a, b, result))
+if __name__ == "__main__":
+    import sys
+    fib(int(sys.argv[1]))
