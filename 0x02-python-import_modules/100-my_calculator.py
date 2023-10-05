52612c90ab9fc73dcf9ce6e7109ad0a19d4237b0
zakaria1011 <grarizakaria@gmail.com>
2023-10-05 23:29:59 +0100

    alx

---
+++ 0x02-python-import_modules/100-my_calculator.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+if __name__ == "__main__":
+    import sys
+    from calculator_1 import add, sub, mul, div
+    if len(sys.argv) - 1 != 3:
+        print("Usage: ./100-my_calculator.py <a> <operator> <b>")
+        sys.exit(1)
+    if (sys.argv[2] == "+"):
+        result = int(sys.argv[1]) + int(sys.argv[3])
+        print("{:s} + {:s} = {:d}".format(sys.argv[1], sys.argv[3], result))
+    elif (sys.argv[2] == "-"):
+        result = int(sys.argv[1]) - int(sys.argv[3])
+        print("{:s} - {:s} = {:d}".format(sys.argv[1], sys.argv[3], result))
+    elif (sys.argv[2] == "*"):
+        result = int(sys.argv[1]) * int(sys.argv[3])
+        print("{:s} * {:s} = {:d}".format(sys.argv[1], sys.argv[3], result))
+    elif (sys.argv[2] == "/"):
+        result = int(sys.argv[1]) / int(sys.argv[3])
+        print("{:s} / {:s} = {:f}".format(sys.argv[1], sys.argv[3], result))
+    else :
+        print("Unknown operator. Available operators: +, -, * and /")
