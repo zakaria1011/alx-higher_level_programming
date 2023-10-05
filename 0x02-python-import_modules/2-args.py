81321eaf88d13dede11a7d0d096d6e5dbb93b018
zakaria1011 <grarizakaria@gmail.com>
2023-10-05 14:32:31 +0100

    alx

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -3,11 +3,12 @@ if __name__ == "__main__":
     import sys
     args = sys.argv[1:]
     args_len = len(args)
+    i = 1
     if args_len == 0:
         print("{:d} arguments.".format(args_len))
     elif args_len == 1:
         print("{:d} argument:".format(args_len))
-        print("1 {:s}".format(sys.argv[1]))
+        print("{:d} {:s}".format(i, sys.argv[1]))
     else:
         print("{:d} arguments:".format(args_len))
         for i in range(1, args_len):
