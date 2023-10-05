573cc7ebc82d26feb617b2390d3ca2de8884da93
zakaria1011 <grarizakaria@gmail.com>
2023-10-05 14:35:52 +0100

    alx

--- 0x02-python-import_modules/2-args.py
+++ 0x02-python-import_modules/2-args.py
@@ -11,6 +11,5 @@ if __name__ == "__main__":
         print("{:d}: {:s}".format(index, sys.argv[1]))
     else:
         print("{:d} arguments:".format(argv_count))
-        while index <= argv_count:
+        for index in range(1, argv_count):
             print("{:d}: {:s}".format(index, sys.argv[index]))
-            index += 1
