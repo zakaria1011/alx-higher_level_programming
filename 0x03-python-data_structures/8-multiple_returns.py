0770f558c12abe43d62213ee0b72b779260eb58c
zakaria1011 <grarizakaria@gmail.com>
2023-10-08 16:15:09 +0100

    alx

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,5 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
-    if sentence is None:
-        return None
     list_string = list(sentence)
     leng_of_string = len(list_string)
     return (leng_of_string, list_string[0])
