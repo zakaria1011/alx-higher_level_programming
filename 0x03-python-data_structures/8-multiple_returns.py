db2c699ccdd9494313e0b15ba9ac79b45ca365a0
zakaria1011 <grarizakaria@gmail.com>
2023-10-08 16:21:08 +0100

    alx

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence = "":
-        sentence[0] = None
+       first_char = None
     else:
-        return (len(sentence), sentence[0])
+        first_char = sentence[0]
+    return (len(sentence), sentence[0])
