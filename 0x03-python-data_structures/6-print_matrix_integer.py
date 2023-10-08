6826e7a7ece032d02e7bb69dec0792c1a2809e57
zakaria1011 <grarizakaria@gmail.com>
2023-10-08 14:56:07 +0100

    amx

---
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -0,0 +1,8 @@
+#!/usr/bin/python3
+def print_matrix_integer(matrix=[[]]):
+    rows = len(matrix)
+    columns = len(matrix[0])
+    for i in range(rows):
+        for j in range(columns):
+            print("{:d} ".format(matrix[i][j]), end = "")
+        print("\n")
