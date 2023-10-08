6d8cee2c7a3c991b235526b9d9883b6bfd5ae52f
zakaria1011 <grarizakaria@gmail.com>
2023-10-08 15:27:50 +0100

    alx

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -8,15 +8,3 @@ def print_matrix_integer(matrix=[[]]):
             if j != columns - 1:
                 print(" ", end="")
         print()
-
-
-
-matrix = [
-    [1, 2, 3],
-    [4, 5, 6],
-    [7, 8, 9]
-]
-
-print_matrix_integer(matrix)
-print("--")
-print_matrix_integer()
