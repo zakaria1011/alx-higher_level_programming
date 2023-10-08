31faac46100aa88149ae7a7f0cc7346fc95cbb97
zakaria1011 <grarizakaria@gmail.com>
2023-10-08 16:22:18 +0100

    alx

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     if sentence = "":
-       first_char = None
+        first_char = None
     else:
         first_char = sentence[0]
     return (len(sentence), sentence[0])
