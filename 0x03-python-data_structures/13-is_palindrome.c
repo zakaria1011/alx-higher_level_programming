b8e0af6afd544c2da07d62db2c7f16fbbf9c509d
zakaria1011 <grarizakaria@gmail.com>
2023-10-11 00:11:00 +0100

    alx

---
+++ 0x03-python-data_structures/13-is_palindrome.c
@@ -0,0 +1,66 @@
+#include "lists.h"
+int num_of_nods(listint_t *head)
+{
+int count = 0;
+listint_t *current = head;
+while (current != NULL)
+{
+    current = current->next;
+    count++;
+}
+return (count);
+}
+int is_palindrome(listint_t **head)
+{
+    listint_t *current;
+    int count = 0;
+    int mid, half1 = 0 ,half2 = 0;
+    int i;
+    current = *head;
+    count = num_of_nods(current);
+    if (count % 2 == 0)
+    {
+        mid = count / 2;
+        for (i = 1; i <= mid ; i++)
+        {
+            half1 += current->n;
+            current = current->next;
+        }
+        for (i = (mid + 1); i <= (2*mid) ; i++)
+        {
+            half2 = half2 + ((-1)*current->n);
+            current = current->next;
+        }
+        if (half1 + half2 == 0)
+        {
+            return (1);
+        }
+        else
+        {
+            return (0);
+        }
+    }
+    else
+    {
+        mid  = count / 2;
+        for (i = 1; i <= mid ; i++)
+        {
+            half1 += current->n;
+            current = current->next;
+        }
+        for (i = (mid + 1) ; i <= (2*mid) ; i++)
+        {
+            half2 = half2 + ((-1)*current->n);
+            current = current->next;
+        }
+        if (half1 + half2 == 0)
+        {
+            return (1);
+        }
+        else
+        {
+            return (0);
+        }
+    }
+    
+}
