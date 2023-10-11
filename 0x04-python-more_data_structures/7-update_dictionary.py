a51c87f416fc249b1dfa4c549abc9009f20e3bf4
zakaria1011 <grarizakaria@gmail.com>
2023-10-11 20:46:22 +0100

    alx

--- 0x04-python-more_data_structures/7-update_dictionary.py
+++ 0x04-python-more_data_structures/7-update_dictionary.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
 def update_dictionary(a_dictionary, key, value):
-    if not isinstance(a_dictionary, dict):
-        a_dictionary[key] = value
-return a_dictionary
+    a_dictionary[key] = value
+    return a_dictionary
