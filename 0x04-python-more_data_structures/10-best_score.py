2499966cec125b19c74e49217df549e68592866b
zakaria1011 <grarizakaria@gmail.com>
2023-10-11 21:53:52 +0100

    alx

--- 0x04-python-more_data_structures/10-best_score.py
+++ 0x04-python-more_data_structures/10-best_score.py
@@ -1,6 +1,11 @@
 #!/usr/bin/python3
 def best_score(a_dictionary):
-    if not isinstance(a_dictionary, dict) or len(a_dictionary) == 0:
+    if a_dictionary is None:
         return None
-    max_key = max(a_dictionary)
-    return (max_key)
+    best_student = next(iter(a_dictionary))
+    best_score = a_dictionary[best_student]
+    for student, score in a_dictionary.items():
+        if score > best_score:
+            best_score = score
+            best_student = student
+    return best_student
