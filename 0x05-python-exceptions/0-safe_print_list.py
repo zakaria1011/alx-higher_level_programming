75642406bcfb195eb8cd59262603f97914bf5799
zakaria1011 <grarizakaria@gmail.com>
2023-10-23 20:16:03 +0100

    alx

---
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+def safe_print_list(my_list=[], x=0):
+    try:
+        printed_elements = 0
+        for i in range(x):
+            print(my_list[i], end="")
+            printed_elements += 1
+    except IndexError:
+        pass
+    print()
+    return printed_elements
