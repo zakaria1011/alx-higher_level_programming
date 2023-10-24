3fe88333133cdb37613c573171b028eb5facb183
zakaria1011 <grarizakaria@gmail.com>
2023-10-24 22:06:20 +0100

    alx

--- 0x06-python-classes/4-square.py
+++ 0x06-python-classes/4-square.py
@@ -30,9 +30,9 @@ class Square:
     @size.setter 
     def size(self, value):
         """ control the type of the velue """
-        if not isinstance(size, int):
+        if not isinstance(value, int):
             raise TypeError("size must be an integer")
-        elif size < 0:
+        elif value < 0:
             raise ValueError("size must be >= 0")
         self.__size = value
 
