c8738e28c7a0e409d04d4843051abc9115d241c2
zakaria1011 <grarizakaria@gmail.com>
2023-10-25 00:06:59 +0100

    alx

--- 0x06-python-classes/6-square.py
+++ 0x06-python-classes/6-square.py
@@ -63,7 +63,7 @@ class Square:
             print()
             return
         else:
-            for i in range(self.position[1]):
+            for _ in range(self.position[1]):
                 print()
-            for i in range(self.size):
+            for _ in range(self.size):
                 print(" " * self.position[0] + "#" * self.size)
