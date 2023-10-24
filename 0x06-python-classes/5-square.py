5f1e3a64e81ea4116415399a1ac9beb83b19f77c
zakaria1011 <grarizakaria@gmail.com>
2023-10-24 23:08:06 +0100

    alx

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,51 @@
+#!/usr/bin/python3
+
+""" Define a classe square """
+
+
+class Square:
+    """
+    This class defines a square.
+
+    Attributes:
+        __size (int): The size of the square.
+
+    Methods:
+        __init__(self, size=0): Initializes a new Square object
+    """
+    def __init__(self, size=0):
+        """
+        Initializes a new Square object.
+
+        Args:
+            size (int): The size of the square.
+        """
+        self.__size = size
+
+    @property
+    def size(self):
+        """ retrieve the size of square)"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """ control the type of the velue """
+        if not isinstance(value, int):
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        self.__size = value
+
+    def area(self):
+        """ Return area of the square """
+        return self.__size ** 2
+
+    def my_print(self):
+        """ Print the square with #"""
+        if self.__size == 0:
+            print()
+        else:
+            for i in range(self.size):
+                for j in range(self.size):
+                    print("#", end="")
+                print()
