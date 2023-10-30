bed55bae8897e2844fb2b259e52d2650facffd57
zakaria1011 <grarizakaria@gmail.com>
2023-10-30 21:30:32 +0100

    alx

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -23,7 +23,6 @@ class Rectangle:
 
     @width.setter
     def width(self, value):
-        """ setter """
         if not isinstance(value, int):
             raise TypeError("width must be an integer")
         elif value < 0:
@@ -37,7 +36,6 @@ class Rectangle:
 
     @height.setter
     def height(self, value):
-        """ setter """
         if not isinstance(value, int):
             raise TypeError("height must be an integer")
         elif value < 0:
