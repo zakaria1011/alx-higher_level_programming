723e58c8b9d9dc8a777bed69ace4723321fa03bf
zakaria1011 <grarizakaria@gmail.com>
2023-10-30 22:09:38 +0100

    alx

--- 0x08-python-more_classes/1-rectangle.py
+++ 0x08-python-more_classes/1-rectangle.py
@@ -41,3 +41,4 @@ class Rectangle:
             raise TypeError("height must be an integer")
         elif value < 0:
             raise ValueError("height must be >= 0")
+        self.__height = value
