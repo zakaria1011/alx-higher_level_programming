7e6f00df3063e1c28987188bac2a5dee50eb6067
zakaria1011 <grarizakaria@gmail.com>
2023-10-30 22:25:44 +0100

    alx

--- 0x08-python-more_classes/2-rectangle.py
+++ 0x08-python-more_classes/2-rectangle.py
@@ -49,4 +49,4 @@ class Rectangle:
 
     def perimeter(self):
         """ return perimeter of rectangle """
-        return 2 * (self.__height + self.__width)
+        return (2 * (self.__height + self.__width))
