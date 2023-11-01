e53f9bd4dd87feebf520399c71f5395a2d402664
zakaria1011 <grarizakaria@gmail.com>
2023-11-01 13:52:31 +0100

    alx

--- 0x07-python-test_driven_development/4-print_square.py
+++ 0x07-python-test_driven_development/4-print_square.py
@@ -10,7 +10,7 @@ def print_square(size):
     args:
     size (int): size of the squire
     """
-    if not isinstance (size, int):
+    if not isinstance(size, int):
         raise TypeError("size must be an integer")
     if size < 0:
         raise ValueError("size must be >= 0")
