d6ebf1a3f690e04d422b325e41f620c281aafa29
zakaria1011 <grarizakaria@gmail.com>
2023-11-07 00:30:29 +0100

    alx

---
+++ 0x0A-python-inheritance/6-base_geometry.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+
+""" class named Basegeometry"""
+
+
+class BaseGeometry:
+    """ class of base geo """
+    def area(self):
+        """ calcule area of a figure """
+        raise Exception("area() is not implemented")
