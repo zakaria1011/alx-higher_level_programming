f865a09fd2fa1bf4c32ac244d5b8efe19e104c3f
zakaria1011 <grarizakaria@gmail.com>
2023-11-06 22:51:44 +0100

    alx

---
+++ 0x0A-python-inheritance/1-my_list.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+
+""" class inherit from list """
+
+
+class MyList(list):
+    """
+    extend list built-in
+    args : list
+    """
+    def print_sorted(self):
+        """
+        methode that print object
+        return : sorted list
+        """
+        print(self)
