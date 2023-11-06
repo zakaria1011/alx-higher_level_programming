dd858e01bbb85b22eeff911c64bcc1f7e5473e66
zakaria1011 <grarizakaria@gmail.com>
2023-11-07 00:21:38 +0100

    alx

--- 0x0A-python-inheritance/4-inherits_from.py
+++ 0x0A-python-inheritance/4-inherits_from.py
@@ -9,4 +9,4 @@ def inherits_from(obj, a_class):
     obj: object
     a_class : a classe
     """
-    return return isinstance(obj, a_class) and obj.__class__ != a_class
+    return isinstance(obj, a_class) and obj.__class__ != a_class
