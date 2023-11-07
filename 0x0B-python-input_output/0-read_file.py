c90a98e10011f3557ecdc277077124a5c55b0c21
zakaria1011 <grarizakaria@gmail.com>
2023-11-07 15:40:50 +0100

    alx

---
+++ 0x0B-python-input_output/0-read_file.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+
+"""function that open a file """
+
+
+def read_file(filename=""):
+    """ 
+    open a file 
+    args:
+    file 
+    """
+    with open(filename, 'r') as file:
+        for line in file:
+            print(line, end='')
