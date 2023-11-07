44c52caff570c5a3d32b8d6718c1be5a25df4851
zakaria1011 <grarizakaria@gmail.com>
2023-11-07 16:55:51 +0100

    alx

---
+++ 0x0B-python-input_output/5-save_to_json_file.py
@@ -0,0 +1,17 @@
+#!/usr/bin/python3
+
+"""a function that save json to a file"""
+
+import json
+
+
+def save_to_json_file(my_obj, filename):
+    """
+    save a json to a file
+    args:
+    my_obj: object to make json represenation
+    filename: file where to save json
+    """
+    json_representation = json.dumps(my_obj)
+    with open(filename, 'w', encoding='utf-8'):
+        filename.write(json_representation)
