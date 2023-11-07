1ac6f80f7f3d285adb7b0fcdac991d58a95a4876
zakaria1011 <grarizakaria@gmail.com>
2023-11-08 00:08:44 +0100

    alx

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,27 @@
+#!/usr/bin/python3
+
+"""class student"""
+
+
+class Student:
+    def __init__(self, first_name, last_name, age):
+        """ initialisation """
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """ return dictionnary """
+        if attrs is None:
+            return self.__dict__
+        result = {}
+        for attr in attrs:
+            if attr in self.__dict__:
+                result[attr] = self.__dict__[attr]
+        return result
+    
+    def reload_from_json(self, json):
+        """Replace attributes with values from the provided dictionary."""
+        for key, value in json.items():
+            if hasattr(self, key):
+                setattr(self, key, value)
