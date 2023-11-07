a998feb1754ca3bb2d02599b41756aca3ed1195c
zakaria1011 <grarizakaria@gmail.com>
2023-11-07 01:32:34 +0100

    alx

---
+++ 0x0A-python-inheritance/100-my_int.py
@@ -0,0 +1,14 @@
+#!/bin/usr/python3
+
+""" myint inheret from int  """
+
+
+class MyInt(int):
+    """class my int  """
+    def __eq__(self, other):
+        """ Override the equality operator (==) """
+        return super().__ne__(other)
+
+    def __ne__(self, other):
+        """ Override the inequality operator (!=) """
+        return super().__eq__(other)
