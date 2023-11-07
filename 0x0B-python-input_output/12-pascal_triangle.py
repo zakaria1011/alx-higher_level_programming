08058ee7940dec1c4d03e8bd7452c7837f74aa83
zakaria1011 <grarizakaria@gmail.com>
2023-11-08 00:55:27 +0100

    alx

---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+
+""" pascale triangle """
+
+
+def fact(x):
+    if x == 0:
+        return 1
+    return x * fact(x - 1)
+
+
+def pascal_triangle(n):
+    result = []
+    if n <= 0:
+        return result
+    for i in range(n + 1):
+        lists = []
+        for k in range(i + 1):
+            elem_list = int(fact(i)/(fact(k) * fact(i - k)))
+            lists.append(elem_list)
+        result.append(lists)
+    return result
