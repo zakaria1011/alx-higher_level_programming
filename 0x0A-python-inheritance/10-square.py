bd373439555297ab66e9f989b097dd6fd4ea2796
zakaria1011 <grarizakaria@gmail.com>
2023-11-07 01:03:30 +0100

    alx

---
+++ 0x0A-python-inheritance/10-square.py
@@ -0,0 +1,11 @@
+#!/usr/bin/python3
+""" class squar inherite from BaseGeo """
+BaseGeometry = __import__('7-base_geometry').BaseGeometry
+
+
+class Square(BaseGeometry):
+    """ class Square """
+    def __init__(self, size):
+        """ initiation """
+        self.integer_validator("size", size)
+        self.size = size
