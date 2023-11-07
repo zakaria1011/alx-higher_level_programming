902f9bc479eeced31865dd1ccfb3f7653578720b
zakaria1011 <grarizakaria@gmail.com>
2023-11-07 01:07:58 +0100

    alx

--- 0x0A-python-inheritance/10-square.py
+++ 0x0A-python-inheritance/10-square.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """ class squar inherite from BaseGeo """
-BaseGeometry = __import__('9-rectangle').Rectangle
+Rectangle = __import__('9-rectangle').Rectangle
 
 
 class Square(Rectangle):
