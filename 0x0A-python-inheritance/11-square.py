592ca7ed4af25e8f9a6bba088c44c9000b3661b2
zakaria1011 <grarizakaria@gmail.com>
2023-11-07 01:14:39 +0100

    alx

---
+++ 0x0A-python-inheritance/11-square.py
@@ -0,0 +1,16 @@
+#!/usr/bin/python3
+""" class squar inherite from BaseGeo """
+Rectangle = __import__('9-rectangle').Rectangle
+
+
+class Square(Rectangle):
+    """ class Square """
+    def __init__(self, size):
+        """ initiation """
+        self.integer_validator("size", size)
+        self.__size = size
+        super().__init__(size, size)
+
+    def __str__(self):
+        """ Return the square description as a string """
+        return "[Squar] {}/{}".format(self.__size, self.__size)
