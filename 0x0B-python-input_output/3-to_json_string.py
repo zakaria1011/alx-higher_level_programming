e065d0f89ae6882ac300ebe428e8e61cc1f6f896
zakaria1011 <grarizakaria@gmail.com>
2023-11-07 16:26:34 +0100

    alx

---
+++ 0x0B-python-input_output/3-to_json_string.py
@@ -0,0 +1,10 @@
+#!/usr/bin/python3
+import json
+""" function that return json representation of an object"""
+
+
+def to_json_string(my_obj):
+    """
+    return json recpresenation of object
+    """
+    return json.dumps(my_obj)
