efd51b02d7b34d0e6b8340c7636f6838d14caf6e
zakaria1011 <grarizakaria@gmail.com>
2023-11-07 15:57:29 +0100

    alx

---
+++ 0x0B-python-input_output/1-write_file.py
@@ -0,0 +1,19 @@
+#!/usr/bin/python3
+
+""" function that write in file  """
+
+
+def write_file(filename="", text=""):
+    """
+    write in a file 
+    return number of caractere writen
+    args: 
+    filename : file 
+    text: text to write
+    """
+    with open (filename, 'w', encoding = 'utf-8') as file:
+        file.write(text)
+    with open (filename, 'r') as file:
+        content = file.read()
+        num_of_carac = len(content)
+    return num_of_carac
