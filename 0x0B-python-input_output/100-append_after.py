8922bbe9fd8bfe70e659c235bc651b1e7e0f3d71
zakaria1011 <grarizakaria@gmail.com>
2023-11-08 01:16:10 +0100

    alx

---
+++ 0x0B-python-input_output/100-append_after.py
@@ -0,0 +1,21 @@
+#!/usr/bin/python3
+
+"""fonction insert text after specific string"""
+
+
+def append_after(filename="", search_string="", new_string=""):
+    if not filename or not search_string or not new_string
+    return
+
+    file_lines = []
+    with open(filename, 'r') as file:
+        file_lines = file.readlines()
+
+    new_file_lines = []
+    for line in file_lines:
+        new_file_lines.append(line)
+        if search_string in line:
+            new_file_lines.append(new_string)
+
+    with open(filename, 'w') as file:
+        file.writelines(new_file_lines)
