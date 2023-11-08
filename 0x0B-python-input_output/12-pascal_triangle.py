e391453069c1678d939430b3b1eb20d5a52c1597
zakaria1011 <grarizakaria@gmail.com>
2023-11-08 15:47:23 +0100

    amx

--- 0x0B-python-input_output/12-pascal_triangle.py
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -7,6 +7,7 @@ def fact(x):
     """ factorial """
     if x == 0:
         return 1
+    
     return x * fact(x - 1)
 
 
@@ -17,7 +18,7 @@ def pascal_triangle(n):
         return result
     for i in range(n):
         lists = []
-        for k in range(i):
+        for k in range(i + 1):
             elem_list = int(fact(i)/(fact(k) * fact(i - k)))
             lists.append(elem_list)
         result.append(lists)
