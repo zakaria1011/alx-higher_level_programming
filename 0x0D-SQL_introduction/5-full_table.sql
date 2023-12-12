232b8fa3accbe7d2aae00e25b7edf1656cc3ee25
zakaria1011 <grarizakaria@gmail.com>
2023-12-12 23:47:54 +0100

    alx

--- 0x0D-SQL_introduction/5-full_table.sql
+++ 0x0D-SQL_introduction/5-full_table.sql
@@ -1,14 +1,2 @@
 -- Check if the database name is provided
-SET @database_name = 'hbtn_0c_0'; 
-SELECT
-    COLUMN_NAME,
-    COLUMN_TYPE,
-    IS_NULLABLE,
-    COLUMN_KEY,
-    COLUMN_DEFAULT,
-    EXTRA
-FROM
-    INFORMATION_SCHEMA.COLUMNS
-WHERE
-    TABLE_SCHEMA = @database_name
-    AND TABLE_NAME = 'first_table';
+SHOW CREATE table first_table;
