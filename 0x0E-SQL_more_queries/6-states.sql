e3dcce290e8ffd0c527e3db8141aace68c852f0f
zakaria1011 <grarizakaria@gmail.com>
2023-12-13 15:10:56 +0100

    alx

---
+++ 0x0E-SQL_more_queries/6-states.sql
@@ -0,0 +1,7 @@
+-- Create database hbtn_0d_usa if not exists
+CREATE DATABASE IF NOT EXISTS hbtn_0d_usa;
+USE hbtn_0d_usa;
+CREATE TABLE IF NOT EXISTS states (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    name VARCHAR(256) NOT NULL
+);
