0573bfbff67cfce0ebcf4a6068de2b73d5949a63
GRARI ZAKARIA <grari.zakaria@taalim.ma>
2024-01-08 21:09:32 +0100

    alx

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,6 +1,5 @@
 #!/usr/bin/node
-const firstArg = process.argv[2];
-const x = parseInt(firstArg);
+const x = parseInt(process.argv[2]);
 if (!isNaN(x)) {
   for (let i = 0; i < x; i++) {
     console.log('C is fun');
