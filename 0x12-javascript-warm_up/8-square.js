bc8a27e9a95ed17c7da0075dd49f4911c4c1f6d7
GRARI ZAKARIA <grari.zakaria@taalim.ma>
2024-01-08 21:29:29 +0100

    alx

--- 0x12-javascript-warm_up/8-square.js
+++ 0x12-javascript-warm_up/8-square.js
@@ -1,11 +1,9 @@
-//#!/usr/bin/node
-const x = Math.floor(Number(process.argv[2]));
-if (isNaN(x)) {
+#!/usr/bin/node
+const size = Math.floor(Number(process.argv[2]));
+if (isNaN(size)) {
   console.log('Missing size');
 } else {
-  for (let i = 0; i < x; i++) {
-    for (let j = 0 ; j < x; j++){
-      console.log('X');
-    }
+  for (let i = 0; i < size; i++) {
+    console.log('X'.repeat(size));
   }
 }
