4b77305c018014d6b0de9c3aea4859fa0fcc6bff
zakaria1011 <grarizakaria@gmail.com>
2024-01-08 16:16:54 +0100

    alx

--- 0x12-javascript-warm_up/2-arguments.js
+++ 0x12-javascript-warm_up/2-arguments.js
@@ -1,1 +1,10 @@
-const argsCount = process.argv.lenght - 2 ; 
+#!/usr/local/bin/node
+const argsCount = process.argv.length - 2;
+
+if (argsCount === 0) {
+  console.log('No argument');
+} else if (argsCount === 1) {
+  console.log('Argument found');
+} else {
+  console.log('Arguments found');
+}
