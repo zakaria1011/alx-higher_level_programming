2823a04e97fe8729a0be3ab595ec8e829a0d4925
GRARI ZAKARIA <grari.zakaria@taalim.ma>
2024-01-08 20:19:21 +0100

    alx

--- 0x12-javascript-warm_up/6-multi_languages_loop.js
+++ 0x12-javascript-warm_up/6-multi_languages_loop.js
@@ -3,7 +3,6 @@ const arrayString = [];
 arrayString[0] = 'C is fun';
 arrayString[1] = 'Python is cool';
 arrayString[2] = 'JavaScript is amazing';
-for (let i = 0 ; i < arrayString.length ; i++)
-{
-    console.log(arrayString[i]);
+for (let i = 0; i < arrayString.length; i++) {
+  console.log(arrayString[i]);
 }
