f77fd2b201eb63dcc144fc0cd1d45b98056cf7e3
GRARI ZAKARIA <grari.zakaria@taalim.ma>
2024-01-08 21:15:32 +0100

    alx

--- 0x12-javascript-warm_up/7-multi_c.js
+++ 0x12-javascript-warm_up/7-multi_c.js
@@ -1,9 +1,9 @@
 #!/usr/bin/node
-const x = parseInt(Math.floor(Number(process.argv[2])));
-if (!isNaN(x)) {
+const x = Math.floor(Number(process.argv[2]));
+if (isNaN(x)) {
+  console.log('Missing number of occurrences');
+} else {
   for (let i = 0; i < x; i++) {
     console.log('C is fun');
   }
-} else {
-  console.log('Missing number of occurrences');
 }
