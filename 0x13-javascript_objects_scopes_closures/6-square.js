a94e9aa875893ff829d60271075db8a3ad742644
zakaria1011 <grarizakaria@gmail.com>
2024-01-09 23:05:12 +0100

    alx

---
+++ 0x13-javascript_objects_scopes_closures/6-square.js
@@ -0,0 +1,18 @@
+#!/usr/bin/node
+const Square1 = require('./5-square');
+class Square extends Square1 {
+  constructor (size) {
+    super(size);
+  }
+
+  charPrint (c) {
+    if (c === undefined) {
+      this.print();
+    } else {
+      for (let i = 0; i < this.height; i++) {
+        console.log(c.repeat(this.width));
+      }
+    }
+  }
+}
+module.exports = Square;
