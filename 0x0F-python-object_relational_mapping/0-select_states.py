a3a993b5162cabdb89bf39182a910999a05dc034
zakaria1011 <grarizakaria@gmail.com>
2024-01-12 09:25:35 +0100

    alx

--- 0x0F-python-object_relational_mapping/0-select_states.py
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -26,7 +26,7 @@ def list_states(username, password, database):
 
 
 if __name__ == "__main__":
-    
+
     if len(sys.argv) != 4:
         print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
         sys.exit(1)
