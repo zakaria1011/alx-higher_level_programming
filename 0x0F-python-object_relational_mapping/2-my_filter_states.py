e334a6dfa969907018c6550297cdcb8c631e9f50
zakaria1011 <grarizakaria@gmail.com>
2024-01-12 22:03:26 +0100

    alx

--- 0x0F-python-object_relational_mapping/2-my_filter_states.py
+++ 0x0F-python-object_relational_mapping/2-my_filter_states.py
@@ -8,8 +8,7 @@ if __name__ == "__main__":
         port=3306,
         user=sys.argv[1],
         passwd=sys.argv[2],
-        db=sys.argv[3],
-    )
+        db=sys.argv[3],)
     cursor = db.cursor()
     query = ("SELECT * FROM states "
              "WHERE name = '{}' "
