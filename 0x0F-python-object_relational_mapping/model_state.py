81352feee9ce3617d746c44ad66dca16270d06dd
zakaria1011 <grarizakaria@gmail.com>
2024-01-13 19:20:00 +0100

    alx

--- 0x0F-python-object_relational_mapping/model_state.py
+++ 0x0F-python-object_relational_mapping/model_state.py
@@ -1,15 +1,16 @@
 #!/usr/bin/python3
-"""Start link class to table in database."""
+"""Start link class to table in database 
+"""
 import sys
 from sqlalchemy import Column, Integer, String, create_engine
 from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
 
 Base = declarative_base()
 
-if __name__ == "__main__":
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-            sys.argv[1], sys.argv[2], sys.argv[3]),
-        pool_pre_ping=True
-    )
-    Base.metadata.create_all(engine)
+class State(Base):
+    """State class"""
+    __tablename__ = 'states'
+
+    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    name = Column(String(128), nullable=False)
