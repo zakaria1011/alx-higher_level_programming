e714682511f4c70e0aeca3fe9e6d37b420c98fc7
zakaria1011 <grarizakaria@gmail.com>
2024-01-13 16:27:54 +0100

    alx

---
+++ 0x0F-python-object_relational_mapping/5-filter_cities.py
@@ -0,0 +1,43 @@
+#!/usr/bin/python3
+"""
+Lists all cities of a given state from the database hbtn_0e_4_usa
+"""
+
+import MySQLdb
+import sys
+
+if __name__ == "__main__":
+    if len(sys.argv) != 5:
+        sys.exit(1)
+
+    username, password, database, state_name = sys.argv[1], \
+        sys.argv[2], sys.argv[3], sys.argv[4]
+
+    db = MySQLdb.connect(
+        host="localhost",
+        port=3306,
+        user=username,
+        passwd=password,
+        db=database,
+        charset="utf8"
+    )
+
+    cursor = db.cursor()
+
+    query = """
+    SELECT GROUP_CONCAT(cities.name ORDER BY cities.id ASC SEPARATOR ', ')
+    FROM cities
+    JOIN states ON cities.state_id = states.id
+    WHERE states.name = %s
+"""
+    cursor.execute(query, (state_name,))
+
+    result = cursor.fetchone()[0]
+
+    if result:
+        print(result)
+    else:
+        sys.exit(0)
+
+    cursor.close()
+    db.close()
