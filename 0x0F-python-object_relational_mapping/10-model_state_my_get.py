6951d0e87dd69db0814b39aa7fa04b72abd7e0dd
zakaria1011 <grarizakaria@gmail.com>
2024-01-14 11:26:36 +0100

    alx

--- 0x0F-python-object_relational_mapping/10-model_state_my_get.py
+++ 0x0F-python-object_relational_mapping/10-model_state_my_get.py
@@ -1,6 +1,7 @@
 #!/usr/bin/python3
 """
-Prints the State object with the name passed as an argument from the database hbtn_0e_6_usa
+Prints the State object with the name passed
+as an argument from the database hbtn_0e_6_usa
 """
 import sys
 from sqlalchemy import create_engine
