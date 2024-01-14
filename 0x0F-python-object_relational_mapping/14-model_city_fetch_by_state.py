efb080bf78067f12322e9b654ee305e63f8fd85d
zakaria1011 <grarizakaria@gmail.com>
2024-01-14 12:23:19 +0100

    alx

--- 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
+++ 0x0F-python-object_relational_mapping/14-model_city_fetch_by_state.py
@@ -1,10 +1,8 @@
 #!/usr/bin/python3
-"""
-Prints all City objects from the database hbtn_0e_14_usa
-"""
+"""Script that prints all City objects from the database hbtn_0e_14_usa."""
 import sys
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import Session
 from model_state import Base, State
 from model_city import City
 
@@ -12,19 +10,16 @@ if __name__ == "__main__":
     if len(sys.argv) != 4:
         sys.exit(1)
 
-    engine = create_engine(
-        'mysql+mysqldb://{}:{}@localhost:3306/{}'.format(
-            sys.argv[1], sys.argv[2], sys.argv[3]
-        ),
-        pool_pre_ping=True
-    )
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost:3306/{}'.format(username, password, database))
 
-    Session = sessionmaker(bind=engine)
-    session = Session()
+    Base.metadata.create_all(engine)
 
-    cities = session.query(City).order_by(City.id).all()
+    session = Session(engine)
+    query = session.query(State, City).join(City).order_by(City.id)
+    result = query.all()
 
-    for city in cities:
-        print('{}: ({}) {}'.format(city.state.name, city.id, city.name))
+    for state, city in result:
+        print("{}: ({}) {}".format(state.name, city.id, city.name))
 
     session.close()
--- 0x0F-python-object_relational_mapping/model_city.py
+++ 0x0F-python-object_relational_mapping/model_city.py
@@ -1,16 +1,11 @@
 #!/usr/bin/python3
-"""
-Contains the class definition of a City.
-"""
+"""Defines the City class."""
 from sqlalchemy import Column, Integer, String, ForeignKey
-from sqlalchemy.orm import relationship
-from model_state import Base
+from model_state import Base, State
 
 class City(Base):
-    """City class"""
+    """Represents a city for a MySQL database."""
     __tablename__ = 'cities'
-
-    id = Column(Integer, primary_key=True, nullable=False, autoincrement=True)
+    id = Column(Integer, primary_key=True, nullable=False, unique=True, autoincrement=True)
     name = Column(String(128), nullable=False)
     state_id = Column(Integer, ForeignKey('states.id'), nullable=False)
-    state = relationship('State', back_populates='cities')
