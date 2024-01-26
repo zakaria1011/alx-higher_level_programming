d08b7e58a8a91008df9ad10e02c93cb52f9e5043
zakaria <grarizakaria@gmail.com>
2024-01-26 12:04:26 +0100

    alx

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -1,12 +1,15 @@
 #!/usr/bin/python3
 """ fetch an url """
-import urllib.request
 
-url = 'https://alx-intranet.hbtn.io/status'
-with urllib.request.urlopen(url) as response:
-    html_content = response.read()
-    utf8_html_content = html_content.decode('utf8')
-    print("Body response:")
-    print("    - type:{}".format(type(html_content)))
-    print("    - content:{}".format(html_content))
-    print("    - utf8 content:{}".format(utf8_html_content))
+
+if __name__ == '__main__':
+    import urllib.request
+
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as response:
+        html_content = response.read()
+        utf8_html_content = html_content.decode('utf8')
+        print("Body response:")
+        print("    - type:{}".format(type(html_content)))
+        print("    - content:{}".format(html_content))
+        print("    - utf8 content:{}".format(utf8_html_content))
