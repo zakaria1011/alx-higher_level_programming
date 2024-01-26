3310e8791f5d18ab6f9b9aa869d08482755d311d
zakaria <grarizakaria@gmail.com>
2024-01-26 12:20:16 +0100

    alx

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -7,7 +7,8 @@ fetch url
 if __name__ == '__main__':
     import urllib.request
 
-    with urllib.request.urlopen('https://alx-intranet.hbtn.io/status') as res:
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as res:
         content = res.read()
         print("Body response:")
         print("\t- type: {}".format(type(content)))
