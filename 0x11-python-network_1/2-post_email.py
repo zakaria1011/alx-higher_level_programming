d52b3fdbb58b0b9baaf872da535307d42cde6b47
zakaria <grarizakaria@gmail.com>
2024-01-26 21:24:55 +0100

    alx

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-""" post email """
+""" post email in usl """
 
 
 if __name__ == '__main__':
@@ -10,7 +10,5 @@ if __name__ == '__main__':
     url = sys.argv[1]
     email = sys.argv[2]
     data = urllib.parse.urlencode({'email': email}).encode('utf-8')
-    req = urllib.request.Request(url, data=data)
-    with urllib.request.urlopen(req) as response:
-        body = response.read().decode('utf-8')
-        print("Your email is:".format(body))
+    with urllib.request.urlopen(url, data=data) as response:
+        print(response.read().decode('utf-8'))
