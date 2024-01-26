c8206015f44cb71aae01935fb76e7b56e3280487
zakaria <grarizakaria@gmail.com>
2024-01-26 18:17:22 +0100

    alx

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,16 +1,17 @@
 #!/usr/bin/python3
-""" post email using only requests"""
+""" post email """
 
-if __name__ == '__main__':
-    import requests
+
+if __name__ = '__main__':
+    import urllib.request
+    import urllib.parse
     import sys
 
     url = sys.argv[1]
     email = sys.argv[2]
-    data = {'email': email}
 
-    response = requests.post(url, data=data)
-
-    content = response.json()
-    email_from_response = content.get('email')
-    print("Your email is: {}".format(email_from_response))
+    data = urllib.parse.urlencode({'email': email}).encode('utf-8')
+    req = urllib.request.Request(url, data)
+    with urllib.request.urlopen(req) as response:
+    body = response.read().decode('utf-8')
+    print(body)
