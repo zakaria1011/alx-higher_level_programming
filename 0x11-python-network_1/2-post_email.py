af4e319dad610cf81a532341c815d1fc204adab3
zakaria <grarizakaria@gmail.com>
2024-01-26 18:24:05 +0100

    alx

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -9,10 +9,8 @@ if __name__ == '__main__':
 
     url = sys.argv[1]
     email = sys.argv[2]
-
     data = urllib.parse.urlencode({'email': email}).encode('utf-8')
     req = urllib.request.Request(url, data=data)
-
     with urllib.request.urlopen(req) as response:
         body = response.read().decode('utf-8')
         print("Your email is:".format(body))
