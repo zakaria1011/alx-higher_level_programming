fd52ae7631c7561c9dba8dafb8a32d9b1b063808
zakaria <grarizakaria@gmail.com>
2024-01-26 16:40:47 +0100

    alx

---
+++ 0x11-python-network_1/3-error_code.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+""" fetch url with error rise """
+
+
+if __name__ == '__main__':
+    import urllib.request
+    import urllib.error
+    import sys
+
+    url = sys.argv[1]
+    try:
+        with urllib.request.urlopen(url) as response:
+            print("{}".format(response.read().decode('utf-8')))
+    except urllib.error.HTTPError as e:
+        print("{}".format(e.code))
