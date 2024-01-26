89545c32b70eaa38f5dcfe7bfd919b4bda2e682e
zakaria <grarizakaria@gmail.com>
2024-01-26 17:24:16 +0100

    alx

---
+++ 0x11-python-network_1/6-post_email.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+""" post email using only requests"""
+
+
+if __name__ == '__main__':
+    import requests
+    import sys
+
+    url = sys.argv[1]
+    email = sys.argv[2]
+    data = {'email': email}
+    response = requests.post(url, data=data)
+    content = response.get('email')
+    print("Your email is: {}".format(content))
