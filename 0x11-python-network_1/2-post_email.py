00dfa7eaef32a06ab5cc0407ce33277ca51b3673
zakaria <grarizakaria@gmail.com>
2024-01-26 17:58:52 +0100

    alx

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -10,12 +10,7 @@ if __name__ == '__main__':
     data = {'email': email}
 
     response = requests.post(url, data=data)
-    
-    # Assuming the server returns JSON and has the email field
-    try:
-        content = response.json()
-        email_from_response = content.get('email')
-        print("Your email is: {}".format(email_from_response))
-    except ValueError:
-        print("Failed to parse JSON response. Response content:")
-        print(response.text)
+
+    content = response.json()
+    email_from_response = content.get('email')
+    print("Your email is: {}".format(email_from_response))
