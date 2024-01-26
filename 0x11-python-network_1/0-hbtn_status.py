f6ca18434ebfb39a49ea70486f4f8330bfa9b046
zakaria <grarizakaria@gmail.com>
2024-01-26 12:15:49 +0100

    alx

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -11,6 +11,6 @@ if __name__ == '__main__':
     with urllib.request.urlopen(url) as response:
         html_content = response.read()
         print("Body response:")
-        print("    - type:{}".format(type(html_content)))
-        print("    - content:{}".format(html_content))
-        print("    - utf8 content:{}".format(html_content.decode('utf8')))
+        print("\t- type:{}".format(type(html_content)))
+        print("\t- content:{}".format(html_content))
+        print("\t- utf8 content:{}".format(html_content.decode('utf8')))
