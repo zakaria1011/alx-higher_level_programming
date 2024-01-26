9c01ab69181754bb71df4b2b318b7cff0c31c916
zakaria <grarizakaria@gmail.com>
2024-01-27 00:16:52 +0100

    alx

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -13,19 +13,13 @@ if __name__ == '__main__':
 
     url = 'http://0.0.0.0:5000/search_user0'
     data = {'q': q}
+    response = requests.post(url, json=data)
 
     try:
-        response = requests.post(url, data=data)
-        response.raise_for_status()
         json_data = response.json()
-        if json_data:
+        if json_data and json_data != {}:
             print("[{}] {}".format(json_data['id'], json_data['name']))
         else:
             print("No result")
-    except requests.exceptions.HTTPError as e:
-        if e.response.status_code == 404:
-            print("No result")
-        else:
-            print("Not a valid JSON")
     except ValueError:
         print("Not a valid JSON")
