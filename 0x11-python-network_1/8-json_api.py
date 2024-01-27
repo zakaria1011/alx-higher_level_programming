1b680172cca8d1c386f1bc3839c5c9f7121b654a
zakaria <grarizakaria@gmail.com>
2024-01-27 15:56:53 +0100

    alx

--- 0x11-python-network_1/8-json_api.py
+++ 0x11-python-network_1/8-json_api.py
@@ -1,25 +1,28 @@
 #!/usr/bin/python3
-""" json api """
+"""
+Script that takes in a letter and sends a POST request 
+"""
 
+import requests
+import sys
 
-if __name__ == '__main__':
-    import requests
-    import sys
-
+if __name__ == "__main__":
     if len(sys.argv) == 1:
         q = ""
     else:
         q = sys.argv[1]
 
-    url = 'http://0.0.0.0:5000/search_user0'
+    url = 'http://0.0.0.0:5000/search_user'
     data = {'q': q}
-    response = requests.post(url, data=data)
 
     try:
-        json_data = response.read()
-        if json_data == {}:
-            print("No result")
+        response = requests.post(url, data=data)
+        json_data = response.json()
+
+        if json_data:
+            print("[{}] {}".format(json_data.get('id'), json_data.get('name')))
         else:
-            print("[{}] {}".format(json_data['id'], json_data['name']))
+            print("No result")
+
     except ValueError:
         print("Not a valid JSON")
