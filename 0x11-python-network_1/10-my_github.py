56c0389dc6a3487c767cd183722ddbc0ce845c19
zakaria <grarizakaria@gmail.com>
2024-01-27 17:02:57 +0100

    alx

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -1,23 +1,25 @@
 #!/usr/bin/python3
 """
-
+Script that takes GitHub credentials (username and personal access token)
+and uses the GitHub API to display the user id.
 """
 
-if __name__ == '__main__':
-    import requests
-    import sys
+import requests
+import sys
 
+if __name__ == "__main__":
     username = sys.argv[1]
     token = sys.argv[2]
-    url = 'https://api.github.com/user' 
+    url = 'https://api.github.com/user'
 
     try:
         response = requests.get(url, auth=(username, token))
         json_data = response.json()
-        
+
         if 'id' in json_data:
             print(json_data['id'])
         else:
             print("None")
-    except ValueEroor:
+
+    except ValueError:
         print("None")
