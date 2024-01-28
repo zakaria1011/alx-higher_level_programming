a8d9dc9a91fce725880d8055090d135e195cd749
zakaria1011 <grarizakaria@gmail.com>
2024-01-28 16:14:59 +0100

    alx

---
+++ 0x11-python-network_1/100-github_commits.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+""" api git hub """
+
+
+import requests
+import sys
+
+if __name__ == '__main__':
+    repo_owner = sys.argv[1]
+    repo_name = sys.argv[2]
+
+    api_url = 'https://developer.github.com/v3/repos/{}/{}/commits/'.format(
+        repo_name, repo_owner)
+    response = requests.get(api_url)
+    commits = response.json()
+    try:
+        for i in range(10):
+            print("{}: {}".format(
+                commits[i].get("sha"),
+                commits[i].get("commit").get("author").get("name")))
+    except IndexError:
+        pass
