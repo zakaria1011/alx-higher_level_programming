609d94e0958cf48d1c717a74f31696980ff65514
zakaria <grarizakaria@gmail.com>
2024-02-20 21:51:03 +0100

    alx

--- 0x14-javascript-web_scraping/4-starwars_count.js
+++ 0x14-javascript-web_scraping/4-starwars_count.js
@@ -12,7 +12,7 @@ request(apiUrl, (error, response, body) => {
     const films = JSON.parse(body).results;
     const wedgeMovies = films.filter(film =>
       film.characters
-      .includes('https://swapi-api.alx-tools.com/api/people/18/')
+        .includes('https://swapi-api.alx-tools.com/api/people/18/')
     );
     console.log(wedgeMovies.length);
   }
