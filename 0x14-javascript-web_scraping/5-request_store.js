3db490c2da39dd7f9a572cd29a43f0ecd127d549
zakaria <grarizakaria@gmail.com>
2024-02-20 21:54:38 +0100

    alx

---
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -0,0 +1,23 @@
+#!/usr/bin/node
+
+const request = require('request');
+const fs = require('fs');
+
+const url = process.argv[2];
+const filePath = process.argv[3];
+
+request(url, (error, response, body) => {
+    if (error) {
+        console.error('Error:', error);
+    } else if (response.statusCode !== 200) {
+        console.error('Status:', response.statusCode);
+    } else {
+        fs.writeFile(filePath, body, { encoding: 'utf-8' }, (err) => {
+            if (err) {
+                console.error('Error writing to file:', err);
+            } else {
+                console.log(`Content saved to ${filePath}`);
+            }
+        });
+    }
+});
