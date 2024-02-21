f934d0631697152455eaa92fab25cb5b02ddc018
zakaria <grarizakaria@gmail.com>
2024-02-21 21:40:00 +0100

    alx

---
+++ 0x14-javascript-web_scraping/101-starwars_characters.js
@@ -0,0 +1,21 @@
+#!/usr/bin/node
+const request = require('request');
+const apiUrl = `https://swapi-api.alx-tools.com/api/films/${process.argv[2]}`;
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error', error);
+    return;
+  }
+  const description = JSON.parse(body);
+  const actors = description.characters;
+  actors.forEach(actor => {
+    request(actor, (error, response, body) => {
+      if (error) {
+        console.error('Error', error);
+        return;
+      }
+      const actorName = JSON.parse(body).name;
+      console.log(actorName);
+    });
+  });
+});
