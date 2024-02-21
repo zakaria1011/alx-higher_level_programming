57a89b3d4f0554d3d759729d82c3de58c71d8e7f
zakaria <grarizakaria@gmail.com>
2024-02-21 19:56:59 +0100

    alx

--- 0x14-javascript-web_scraping/5-request_store.js
+++ 0x14-javascript-web_scraping/5-request_store.js
@@ -7,15 +7,15 @@ const url = process.argv[2];
 const filePath = process.argv[3];
 
 request(url, (error, response, body) => {
-    if (error) {
-        console.error('Error:', error);
-    } else if (response.statusCode !== 200) {
-        console.error('Status:', response.statusCode);
-    } else {
-        fs.writeFile(filePath, body, { encoding: 'utf-8' }, (err) => {
-            if (err) {
-                console.error('Error writing to file:', err);
-            }
-        });
-    }
+  if (error) {
+    console.error('Error:', error);
+  } else if (response.statusCode !== 200) {
+    console.error('Status:', response.statusCode);
+  } else {
+    fs.writeFile(filePath, body, { encoding: 'utf-8' }, (err) => {
+      if (err) {
+        console.error('Error writing to file:', err);
+      }
+    });
+  }
 });
