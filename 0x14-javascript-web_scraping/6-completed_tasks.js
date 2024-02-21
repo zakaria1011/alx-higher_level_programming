c9bb1d5d9f44e9a9f2ab0e316335c9bdb4a153c3
zakaria <grarizakaria@gmail.com>
2024-02-21 20:22:56 +0100

    alx

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,16 +1,22 @@
-const axios = require('axios'); // Make sure to import axios if you're using Node.js or have it available in your environment
+#!/usr/bin/node
+const request = require('request');
 
-axios.get('https://jsonplaceholder.typicode.com/todos')
-    .then(response => {
-        const data = response.data;
-        let completedTasksCount = 0; // Initialize a variable outside the loop to count completed tasks
-        data.forEach(obj => {
-            if(obj.completed === true) {
-                completedTasksCount++; // Increment the count for each completed task
-            }
-        });
-        console.log(`Number of completed tasks: ${completedTasksCount}`);
-    })
-    .catch(error => {
-        console.error('Error fetching data:', error);
-    });
+const apiUrl = process.argv[2];
+request(apiUrl, (error, response, body) => {
+  if (error) {
+    console.error('Error', error);
+    return;
+  }
+  const todos = JSON.parse(body);
+  const completedTask = {};
+  todos.forEach(todo => {
+    if (todo.completed) {
+      if (completedTask[todo.userId]) {
+        completedTask[todo.userId]++;
+      } else {
+        completedTask[todo.userId] = 1;
+      }
+    }
+  });
+  console.log(completedTask);
+});
