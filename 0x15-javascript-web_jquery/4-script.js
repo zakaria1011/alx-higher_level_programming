18f329bc34595f24b86fb0ff5d1da89ebb396c58
zakaria <grarizakaria@gmail.com>
2024-02-28 17:56:23 +0100

    alx

---
+++ 0x15-javascript-web_jquery/4-script.js
@@ -0,0 +1,10 @@
+$(document).ready(function() {
+    $("#toggle_header").click(function() {
+      if ($("header").hasClass("red")) {
+        $("header").removeClass("red").addClass("green");
+      } else {
+        $("header").removeClass("green").addClass("red");
+      }
+    });
+  });
+  
